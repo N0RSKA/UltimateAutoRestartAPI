7dbf3d094b954688d72a83ba721af3899886f565
Norska <norska99@gmail.com>
2022-01-27 21:45:59 +0200

    Build 50b Release

---
+++ src/dev/norska/uar/api/UltimateAutoRestartAPI.java
@@ -0,0 +1,15 @@
+package dev.norska.uar.api;
+
+import dev.norska.uar.UltimateAutoRestart;
+
+public class UltimateAutoRestartAPI {
+	
+	public static int getInterval(UltimateAutoRestart instance) {
+		return 0;
+	}
+	
+	public static void setInterval(UltimateAutoRestart instance, int seconds) {
+		
+	}
+
+}
