47ea8446f262f4bc0f4f4bc88de641e18c09a50f
N0RSKA <contact@norska.dev>
2024-03-02 15:17:48 +0200

    2024.03

--- src/dev/norska/uar/api/UARAPI.java
+++ src/dev/norska/uar/api/UARAPI.java
@@ -1,9 +1,15 @@
 package dev.norska.uar.api;
 
 import dev.norska.uar.UltimateAutoRestart;
+import lombok.Getter;
+import lombok.Setter;
 
 public class UARAPI {
-	
+
+	@Getter
+	@Setter
+	public static String forceRestartReason = "N/A";
+
 	public static int getInterval(UltimateAutoRestart instance) {
 		return 0;
 	}
